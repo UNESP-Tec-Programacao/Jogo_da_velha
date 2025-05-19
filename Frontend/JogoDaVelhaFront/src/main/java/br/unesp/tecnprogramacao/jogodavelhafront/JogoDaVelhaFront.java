4cfc5c8acd4ddf8fd2abff0fde4a5f3577523f78
EduardoMRT <eduksoficial@gmail.com>
2025-05-19 15:11:41 -0300

    ok

--- Frontend/JogoDaVelhaFront/src/main/java/br/unesp/tecnprogramacao/jogodavelhafront/JogoDaVelhaFront.java
+++ Frontend/JogoDaVelhaFront/src/main/java/br/unesp/tecnprogramacao/jogodavelhafront/JogoDaVelhaFront.java
@@ -5,6 +5,7 @@
 package br.unesp.tecnprogramacao.jogodavelhafront;
 
 import br.unesp.tecnprogramacao.jogodavelhafront.exceptions.ThemeException;
+import br.unesp.tecnprogramacao.jogodavelhafront.telas.Inicial;
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -18,9 +19,13 @@ public class JogoDaVelhaFront {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(new FlatMacDarkLaf());
+            Inicial inicial = new Inicial();
+            inicial.setLocationRelativeTo(null); 
+            inicial.setResizable(false);
+            inicial.setVisible(true);
         } catch (UnsupportedLookAndFeelException e) {
             System.err.println("Não foi possível aplicar o FlatLaf");
-            ThemeException(STR."Ocorreu um erro ao tentar iniciar o tema: \{e.getMessage(}");
+            throw new ThemeException("Ocorreu um erro ao tentar iniciar o tema:" + e.getMessage());
         }
     }
 }
