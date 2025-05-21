f709b2ab7b612a77247e4d3c35227e859750d417
Eduardo Miguel dos Reis Teixeira <eduksoficial@gmail.com>
2025-05-20 23:02:36 -0300

    Não precisa desse repository
    O Spring já injeta a dependência automaticamente

--- Backend/jogo-da-velha-back/src/main/java/br/com/unesp/tecnProgramacao/jogo_da_velha_back/repository/JogadorRepository.java
+++ Backend/jogo-da-velha-back/src/main/java/br/com/unesp/tecnProgramacao/jogo_da_velha_back/repository/JogadorRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface JogadorRepository extends JpaRepository<Jogador, UUID> {
     Optional<Jogador> findByNickname(String nickName);
     boolean existsByNickname(String nickName);
